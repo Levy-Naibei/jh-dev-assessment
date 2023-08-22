76ddf6324645d2b02ba5dc9762047c3855fa0382
Levy-Naibei <levynaibei2@gmail.com>
2023-08-22 18:47:58 +0300

    handled empty cells or rows

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ load_dotenv()
 
 sheet_id = os.environ["SHEET_ID"]
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+df.replace(np.nan, '', inplace=True)
 records = df.to_dict(orient="records")
 
 @app.route("/")
@@ -19,6 +21,10 @@ def welcome():
 @app.route("/records")
 def rows():
     return jsonify({"records": records})
+
+@app.route("/load_data", methods=["POST"])
+def load_data():
+    pass
 
 
 if __name__ == "__main__":
