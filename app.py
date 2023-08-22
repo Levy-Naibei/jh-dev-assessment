f01db6ddd100ea200b6f943624dbc9ff5ea229ad
Levy-Naibei <levynaibei2@gmail.com>
2023-08-22 17:05:32 +0300

    built flask server

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import os
+from flask import Flask, jsonify
 import pandas as pd
 from dotenv import load_dotenv
+
+app = Flask(__name__)
 
 # load env varaibles
 load_dotenv()
 
 sheet_id = os.environ["SHEET_ID"]
-print(sheet_id)
 df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
 records = df.to_dict(orient="records")
 
-# print(records)
-# print(df)
+@app.route("/")
+def welcome():
+    return jsonify({"message": "welcome to mentor checklist API!"})
+
+@app.route("/records")
+def rows():
+    return jsonify({"records": records})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
