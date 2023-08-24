da980c82721bdf5977d444180d1412bddf70fec9
Levy-Naibei <levynaibei2@gmail.com>
2023-08-24 03:58:50 +0300

    added Flask-SQLAlchemy ORM

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, jsonify
-import psycopg2
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-import numpy as np
 from dotenv import load_dotenv
 from datetime import datetime
 
@@ -11,96 +10,92 @@ app = Flask(__name__)
 # load env varaibles
 load_dotenv()
 
-sheet_id = os.environ["SHEET_ID"]
-df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
-df.fillna("", inplace=True)
-# records = df.to_dict(orient="records")
+username=os.environ["DB_USER"]
+password=os.environ["DB_PASSWORD"]
+db_name=os.environ["DB_NAME"]
+db_host=os.environ["DB_HOST"]
+db_port=os.environ["DB_PORT"]
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{username}:{password}@{db_host}:{db_port}/{db_name}"
+db = SQLAlchemy(app)
 
+class MentorChecklist(db.Model):
+  __tablename__ = 'mentor_checklist'
+  id = db.Column(db.BigInteger, primary_key=True)
+  cme_completion_date = db.Column(db.Date)
+  cme_topic = db.Column(db.Text)
+  county = db.Column(db.Text)
+  date_submitted = db.Column(db.TIMESTAMP)
+  drill_topic = db.Column(db.Text)
+  # cme_unique_id = db.Column(db.BigInteger)
+  # drill_unique_id = db.Column(db.Text)
+  # essential_cme_topic = db.Column(db.Boolean)
+  # essential_drill_topic = db.Column(db.Boolean)
+  # facility_code = db.Column(db.Text)
+  # facility_name = db.Column(db.Text)
+  # id_number_cme = db.Column(db.Text)
+  # id_number_drill = db.Column(db.Text)
+  mentor_name = db.Column(db.Text)
+  # submission_id = db.Column(db.BigInteger)
+  success_story = db.Column(db.Text)
 
 @app.route("/")
 def welcome():
     return jsonify({"message": "welcome to mentor checklist API!"})
 
 
-# @app.route("/records")
-# def rows():
-#     return jsonify({"records": records})
-
-
 @app.route("/load_data", methods=["POST"])
 def load_data():
-    try:
-        conn = psycopg2.connect(
-            database=os.environ["DB_NAME"],
-            user=os.environ["DB_USER"],
-            password=os.environ["DB_PASSWORD"],
-            host=os.environ["DB_HOST"],
-            port=os.environ["DB_PORT"],
-        )
+  sheet_id = os.environ["SHEET_ID"]
+  df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+  df.fillna("None", inplace=True)
+  
+  # df['essential_cme_topic'] = df['essential_cme_topic'].fillna(False)
+  # df['essential_drill_topic'] = df['essential_drill_topic'].fillna(False)
+  # df['drill_unique_id'] = df['drill_unique_id'].fillna('')
+  # df['cme_unique_id'] = df['cme_unique_id'].fillna(0)
+  # df['id_number_cme'] = df['id_number_cme'].fillna('')
+  # df['id_number_drill'] = df['id_number_drill'].fillna('')
+  # df['facility_code'] = df['facility_code'].split('-')[0]
+  # df['facility_name'] = df['facility_name'].fillna('').split('-')[:]
 
-        cursor = conn.cursor()
+  # Insert data into the database
+  try:
+    for index, row in df.iterrows():
+      cme_completion_date = None
+      if row["mentor_checklist/cme_grp/cme_completion_date"] != 'None':
+         cme_completion_date = datetime.strptime(
+            str(row['mentor_checklist/cme_grp/cme_completion_date']), '%Y-%m-%d').date()
+      else:
+         cme_completion_date = None
+      
+      date_submitted = None
+      if row["_submission_time"] != 'None':
+         date_submitted = datetime.strptime(str(row["_submission_time"]), "%Y-%m-%dT%H:%M:%S")
+      else:
+         date_submitted = None
+      
+      submission_id = None
+      if row['_id'] != "None":
+         submission_id = str(row['_id'])
+      else:
+         submission_id = None
 
-        for index, row in df.iterrows():
-            cme_completion_date = row.get("cme_completion_date", None)
-            if cme_completion_date:
-                cme_completion_date = datetime.strptime(
-                    cme_completion_date, "%Y-%m-%d"
-                ).date()
-
-            date_submitted = row.get("date_submitted", None)
-            if date_submitted:
-                date_submitted = datetime.strptime(date_submitted, "%Y-%m-%dT%H:%M:%S")
-
-            insert_query = """
-            INSERT INTO mentor_checklist (
-                cme_completion_date,
-                cme_topic,
-                cme_unique_id,
-                county,
-                date_submitted,
-                drill_topic,
-                drill_unique_id,
-                essential_cme_topic,
-                essential_drill_topic,
-                facility_code,
-                facility_name,
-                id_number_cme,
-                id_number_drill,
-                mentor_name,
-                submission_id,
-                success_story
-            ) VALUES (
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            )
-            """
-
-            values = (
-                cme_completion_date,
-                row.get("cme_topic", ""),
-                row.get("cme_unique_id", None),
-                row.get("county", ""),
-                date_submitted,
-                row.get("drill_topic", ""),
-                row.get("drill_unique_id", ""),
-                row.get("essential_cme_topic", False),
-                row.get("essential_drill_topic", False),
-                row.get("facility_code", ""),
-                row.get("facility_name", ""),
-                row.get("id_number_cme", ""),
-                row.get("id_number_drill", ""),
-                row.get("mentor_name", ""),
-                row.get("submission_id", None),
-                row.get("success_story", ""),
-            )
-            cursor.execute(insert_query, values)
-
-        conn.commit()
-        cursor.close()
-        conn.close()
-
-        return jsonify({"message": "Data uploaded successfully."})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+      entry = MentorChecklist(
+        id=index,
+        cme_completion_date = cme_completion_date,
+        date_submitted = date_submitted,
+        success_story=row['mentor_checklist/success_grp/story_success'],
+        mentor_name=row['mentor_checklist/mentor/name'],
+        county=row['mentor_checklist/mentor/q_county'],
+        cme_topic=row['mentor_checklist/cme_grp/cme_topics'],
+        drill_topic=row['mentor_checklist/drills_grp/drill_topics'],
+        submission_id=submission_id
+      )
+      db.session.add(entry)
+    db.session.commit()
+    return "Data loaded successfully!"
+  except Exception as e:
+     return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
